8ed67b7c3fbbda1ad0bcd4aea8463c41779fa99b
Dominik <szeplakidominik888@gmail.com>
2024-12-02 20:05:33 +0100

    19. lépés

--- Basics/scripts.js
+++ Basics/scripts.js
@@ -1,66 +1,83 @@
-// function Player(nickname){
-//     this.nickname = nickname
-//     this.playedMatch = 0
-// }
+// // function Player(nickname){
+// //     this.nickname = nickname
+// //     this.playedMatch = 0
+// // }
 
-// Player.prototype.play = 
-// function(){
-//     this.playedMatch++
-//     console.log(this.nickname,this.playedMatch)
-// }
+// // Player.prototype.play = 
+// // function(){
+// //     this.playedMatch++
+// //     console.log(this.nickname,this.playedMatch)
+// // }
 
-// Player.prototype.getTierLevel = function(){
-//     if(this.playedMatch >= 0 && this.playedMatch <= 3){
-//         return 'A'
-//     }else if(this.playedMatch >= 4 && this.playedMatch <= 6){
-//         return 'B'
-//     }else{
-//         return 'C'
+// // Player.prototype.getTierLevel = function(){
+// //     if(this.playedMatch >= 0 && this.playedMatch <= 3){
+// //         return 'A'
+// //     }else if(this.playedMatch >= 4 && this.playedMatch <= 6){
+// //         return 'B'
+// //     }else{
+// //         return 'C'
+// //     }
+
+// // }
+
+// class Player {
+//     constructor(nickname) {
+//         this.nickname = nickname
+//         this.playedMatch = 0
 //     }
 
+//     play() {
+//         this.playedMatch++
+//         console.log(this.nickname, this.playedMatch)
+//     }
+
+//     getTierLevel() {
+//         if (this.playedMatch >= 0 && this.playedMatch <= 3) {
+//             return 'A'
+//         } else if (this.playedMatch >= 4 && this.playedMatch <= 6) {
+//             return 'B'
+//         } else {
+//             return 'C'
+//         }
+//     }
 // }
 
-class Player {
-    constructor(nickname) {
-        this.nickname = nickname
-        this.playedMatch = 0
-    }
 
-    play() {
-        this.playedMatch++
-        console.log(this.nickname, this.playedMatch)
-    }
+// const gomszab = new Player("gomszab")
+// console.log(gomszab);
+// gomszab.play();
+// console.log(gomszab.getTierLevel());
 
-    getTierLevel() {
-        if (this.playedMatch >= 0 && this.playedMatch <= 3) {
-            return 'A'
-        } else if (this.playedMatch >= 4 && this.playedMatch <= 6) {
-            return 'B'
-        } else {
-            return 'C'
-        }
+
+
+// function Person(name){
+//     this.name = name || "Géza";
+// }
+
+// Person.prototype.getName = function(){
+//     return this.name;
+// };
+
+// function Student(name, school){
+//     Person.call(this, name)
+//     this.school = school;
+// }
+
+// Object.setPrototypeOf(Student.prototype, Person.prototype);
+
+class Person {
+    constructor(name = "Géza") {
+        this.name = name;
+    }   
+
+    getName(){
+        return this.name;
     }
 }
 
-
-const gomszab = new Player("gomszab")
-console.log(gomszab);
-gomszab.play();
-console.log(gomszab.getTierLevel());
-
-
-
-function Person(name){
-    this.name = name || "Géza";
+class Student extends Person {
+    constructor(name, school) {
+        super(name);
+        this.school = school;
+    }
 }
-
-Person.prototype.getName = function(){
-    return this.name;
-};
-
-function Student(name, school){
-    Person.call(this, name)
-    this.school = school;
-}
-
-Object.setPrototypeOf(Student.prototype, Person.prototype);
