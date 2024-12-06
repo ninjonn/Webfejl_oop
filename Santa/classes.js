dee17caef877ceeefb01314821bb57449b513ed1
Szeplaki Dominik <szeplaki.dominik@bolyai.local>
2024-12-06 12:14:20 +0100

    classes.js

--- Santa/classes.js
+++ Santa/classes.js
@@ -1,8 +1,27 @@
 
 class Factory{
  // TODO 1, 2, 3, 4, 9, 10
+constructor(){
+    this.manoklist = [] //manoklist tulajdonság létrehozása, azért üres mert még nem tartozik hozzá tag
+}
+
+hozzaAd(companion){ //azért csak a companion kell paraméternek, mert a manoklist elérhető classon belül
+    this.manoklist.push(companion)
+}
 }
 
 class Companion{
  // TODO 5
+ constructor(id,kernev,veznev,reszleg){
+    this.id = id
+    this.kernev = kernev
+    this.veznev = veznev
+    this.reszleg = reszleg
+    this.productlist = [] //mivel nem szükséges a példány létrehozásakor ezért nem a construktor leszármazottja
+ }
+
+ getName(){
+    return this.kernev + this.veznev
+ }
+
 }
