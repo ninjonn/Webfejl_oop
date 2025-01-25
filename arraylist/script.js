6fc813f262c14509b9265e072219a4e67faad30a
Dominik <szeplakidominik888@gmail.com>
2025-01-25 20:57:15 +0100

    .17

--- arraylist/script.js
+++ arraylist/script.js
@@ -1,38 +1,55 @@
-class ArrayList{
+class ArrayList {
     /**
      * @type {Number}
      */
     #length
     #inner
 
-    get Count(){
-        this.#length
+    get Count() {
         return this.#length
     };
+    #htmlArray
 
-    constructor(){
+    constructor(array = undefined) {
         this.#length = 0;
         this.#inner = {};
+        this.#htmlArray = array;
     };
 
-    Add(item){
+    Add(item) {
         const index = this.#length; //elkérjük és eltároljuk az aktuális hosszát
         this.#inner[index] = item; // beállítjuk az index tulajdonságnak a bemeneti paramétert 
-        Object.defineProperty(this,index,{
-            get:function(){
-                return this.#inner[index]
-            },
-            set:function(value){
-               this.#inner[index] = value
-            }
-        })
-        this.#length++; 
+        Object.defineProperty(this, index, {
+            get: () => { return this.#inner[index] },
+            configurable: true,
+            enumerable: true,
+
+            set: (value) => { this.#inner[index] = value },
+            writable: true
+        }
+        );
+        if (this.#htmlArray) {
+            this.#htmlArray.addRow(item);
+        }
+        this.#length++;
     };
 
-    Clear(){
+    Clear() {
+        for (const i in this) {
+            delete this[i];
+        }
         this.#length = 0;//lenullázzuk a hosszt
         this.#inner = {};//objektumot kiürítjük
     };
+
+    Constains(value){
+        for(let key in this.#inner){
+            if(this.#inner[key] === value){
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 const csirke = {};
@@ -43,15 +60,13 @@ console.log(csirke)
 
 
 const alma = {}
-Object.defineProperty(alma,'nev',{
-    value:'Ferenc',
-    writable:true
+Object.defineProperty(alma, 'nev', {
+    value: 'Ferenc',
+    writable: true
 })
 alma.nev = 'asd';
-console.log(alma) 
-    
+console.log(alma)
+
 //1. objektum -> objektum aminek a tulajdonságát megadjuk
 //2.string -> kulcs
 //3.descriptor -> { }
-
-
