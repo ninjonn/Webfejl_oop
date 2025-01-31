9d7bd99c854c7684c7f145ef94832b5bb7faa409
Szeplaki Dominik <szeplaki.dominik@BOLYAI.LOCAL>
2025-01-31 11:30:37 +0100

    valami

---
+++ datamanager/script.js
@@ -0,0 +1,69 @@
+/**
+ * @typedef {{nev:String,eletkor:Number}} Person
+ * @callback UpdateCallback
+ * @param {Person[]} person
+ * @returns {void}
+ */
+class DataManager{
+    /**
+     * @type {Person[]}
+     */
+    #array
+    /**
+     * @type {UpdateCallback}
+     */
+    #updateCallback
+    /**
+     * 
+     * @param {Person[]} sigma 
+     */
+    constructor(sigma = []){
+        this.#array = sigma
+        this.#updateCallback = () => {}
+    }
+    /**
+     * 
+     * @param {UpdateCallback} callback 
+     */
+    setUpdateCallback(callback){
+        this.#updateCallback = callback
+    }
+    /**
+     * 
+     * @param {Person} chad 
+     */
+    add(chad){
+        this.#array.push(chad)  
+        this.#updateCallback(this.#array)  
+    }
+    /**
+     * 
+     * @param {String} sigmaName 
+     */
+    filterName(sigmaName){
+        const result = [];
+        for(const elem of this.#array){
+            if(elem.nev === sigmaName){
+                result.push(elem)
+            }
+        }
+        this.#updateCallback(result)
+    }
+    /**
+     * 
+     * @param {Number} sigmaAge 
+     */
+    filterAge(sigmaAge){
+        const result = [];
+        for(const elem of this.#array){
+            if(elem.eletkor === sigmaAge){
+                result.push(elem)
+            }
+        }
+        this.#updateCallback(result)
+    }
+}
+
+class Datatable{
+    
+}
